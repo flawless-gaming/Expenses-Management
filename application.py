11683356890cc31b63f55db6f55975e5bc66e20d
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-05 16:41:35 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -4,20 +4,20 @@ from PIL import Image, ImageTk
 #creating a application window
 app = Tk()
 app.title("Expense Management")#window title
-app.geometry("300x200")#window size
+app.geometry("600x400")#window size
 app.config(bg="#0e1111")#window colour
 #app.config(bg="#FAF9F6")#window colour
 
 # Read the Image
-image1 = Image.open("daymode.png")
-image2 = Image.open("nightmode.png")
+image1 = Image.open("nightmode.png")
+image2 = Image.open("daymode.png")
 # Resize the image using resize() method
 resize_image1 = image1.resize((75, 25))
 resize_image2 = image2.resize((75, 25))
 daymode = ImageTk.PhotoImage(resize_image1)
 nightmode = ImageTk.PhotoImage(resize_image2)
 
-
+#darkmode and light mode function
 def mode():
     global screenmode
     if screenmode:
@@ -29,13 +29,13 @@ def mode():
         app.config(bg="#0e1111")#window colour
         screenmode = True
 
-screenmode = False
-
+screenmode = True#screenmode set to darkmode
+#screen mode button
 mode_button = Button(app, image = daymode, bd='0',
-                     command = mode ,)
+                     command = mode )
 mode_button.place(x=10,y=5)
 
 
 
-
+#to generate the application
 app.mainloop()
