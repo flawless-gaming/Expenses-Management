c13a1bbf509e451ba8483c5e6e0d3132fb15ed61
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-05 15:49:54 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 #creating a application window
 app = Tk()
@@ -7,8 +8,15 @@ app.geometry("300x200")#window size
 app.config(bg="#0e1111")#window colour
 #app.config(bg="#FAF9F6")#window colour
 
-daymode = PhotoImage(file="daymode.png")
-nightmode = PhotoImage(file="nightmode.png")
+# Read the Image
+image1 = Image.open("daymode.png")
+image2 = Image.open("nightmode.png")
+# Resize the image using resize() method
+resize_image1 = image1.resize((75, 25))
+resize_image2 = image2.resize((75, 25))
+daymode = ImageTk.PhotoImage(resize_image1)
+nightmode = ImageTk.PhotoImage(resize_image2)
+
 
 def mode():
     global screenmode
@@ -24,8 +32,10 @@ def mode():
 screenmode = False
 
 mode_button = Button(app, image = daymode, bd='0',
-                     command = mode)
-mode_button.pack(pady = 50)
+                     command = mode ,)
+mode_button.place(x=10,y=5)
+
+
 
 
 app.mainloop()
