aba2cae5eb356b7b8f582d364c42fc30b46f3979
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-11 11:03:55 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import Image, ImageTk
 
 #creating a application window
@@ -23,17 +24,51 @@ def mode():
     if screenmode:
         mode_button.config(image = nightmode)
         app.config(bg="#FAF9F6")#window colour
+        header.config(bg="#FAF9F6",fg="#0e1111")
         screenmode = False
     else:
         mode_button.config(image = daymode)
         app.config(bg="#0e1111")#window colour
+        header.config(bg="#0e1111",fg="#FAF9F6")
         screenmode = True
+
+def on_enter(e):
+    user.delete(0,'end')
+    
+def on_leave(e):
+    name=user.get()
+    if name == "":
+        user.insert(0,"Username")
+    
 
 screenmode = True#screenmode set to darkmode
 #screen mode button
 mode_button = Button(app, image = daymode, bd='0',
                      command = mode )
 mode_button.place(x=10,y=5)
+
+header = Label(app,text="Login",font=(" Helvetica",25,"bold"),
+               bg="#0e1111",fg="#FAF9F6")
+user = Entry(app,border=0,bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
+user.insert(0,"Username")
+user.bind("<FocusIn>", on_enter)
+user.bind("<FocusOut>",on_leave)
+
+gender = [
+    "Gender",
+    "Male",
+    "Female",
+    "Not Specified"
+]
+clicked=StringVar()
+clicked.set("Gender")
+drop = OptionMenu(app,clicked,*gender)
+drop.config(border=0,bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",10,"bold"))
+
+
+header.place(x=275,y=20)
+user.place(x=220,y=100)
+drop.place(x=240,y=140)
 
 
 
