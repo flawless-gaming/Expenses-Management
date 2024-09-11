6a02bbb96c3d9688a9bd1f18b1081e63894cbc11
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-12 02:19:37 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -6,17 +6,20 @@ from PIL import Image, ImageTk
 app = Tk()
 app.title("Expense Management")#window title
 app.geometry("600x400")#window size
-app.config(bg="#0e1111")#window colour
+app.config(bg="#030f1e")#window colour
 #app.config(bg="#FAF9F6")#window colour
 
 # Read the Image
 image1 = Image.open("nightmode.png")
 image2 = Image.open("daymode.png")
+image3 = Image.open("login_image.jpg")
 # Resize the image using resize() method
 resize_image1 = image1.resize((75, 25))
 resize_image2 = image2.resize((75, 25))
+resize_image3 = image3.resize((200,300))
 daymode = ImageTk.PhotoImage(resize_image1)
 nightmode = ImageTk.PhotoImage(resize_image2)
+loign_image = ImageTk.PhotoImage(resize_image3)
 
 #darkmode and light mode function
 def mode():
@@ -24,12 +27,24 @@ def mode():
     if screenmode:
         mode_button.config(image = nightmode)
         app.config(bg="#FAF9F6")#window colour
-        header.config(bg="#FAF9F6",fg="#0e1111")
+        header.config(bg="#FAF9F6",fg="#030f1e")
+        user.config(bg="#FAF9F6",fg="#030f1e")
+        drop.config(bg="#FAF9F6",fg="#030f1e")
+        drop["menu"].config(bg="#FAF9F6",fg="#030f1e")
+        age.config(bg="#FAF9F6",fg="#030f1e")
+        spinbox.config(bg="#FAF9F6",fg="#030f1e",buttonbackground="#FAF9F6")
+        underline.config(bg="#030f1e")
         screenmode = False
     else:
         mode_button.config(image = daymode)
-        app.config(bg="#0e1111")#window colour
-        header.config(bg="#0e1111",fg="#FAF9F6")
+        app.config(bg="#030f1e")#window colour
+        header.config(bg="#030f1e",fg="#FAF9F6")
+        user.config(bg="#030f1e",fg="#FAF9F6")
+        drop.config(bg="#030f1e",fg="#FAF9F6")
+        drop["menu"].config(bg="#030f1e",fg="#FAF9F6")
+        age.config(bg="#030f1e",fg="#FAF9F6")
+        spinbox.config(bg="#030f1e",fg="#FAF9F6",buttonbackground="#030f1e")
+        underline.config(bg="#FAF9F6")
         screenmode = True
 
 def on_enter(e):
@@ -46,10 +61,11 @@ screenmode = True#screenmode set to darkmode
 mode_button = Button(app, image = daymode, bd='0',
                      command = mode )
 mode_button.place(x=10,y=5)
-
+image_label = Label(app,image = loign_image)
+image_label.place(x=30,y=50)
 header = Label(app,text="Login",font=(" Times New Roman",25,"bold"),
-               bg="#0e1111",fg="#FAF9F6")
-user = Entry(app,border=0,bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
+               bg="#030f1e",fg="#FAF9F6")
+user = Entry(app,border=0,bg="#030f1e",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
 user.insert(0,"Username")
 user.bind("<FocusIn>", on_enter)
 user.bind("<FocusOut>",on_leave)
@@ -63,21 +79,23 @@ gender = [
 clicked=StringVar()
 clicked.set("Gender")
 drop = OptionMenu(app,clicked,*gender)
-drop.config(border=0,bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
-drop["menu"].config(border=0,bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
-age = Label(app,text="Age",bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
+drop.config(border=0,bg="#030f1e",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
+drop["menu"].config(border=0,bg="#030f1e",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
+age = Label(app,text="Age",bg="#030f1e",fg="#FAF9F6",font=("Microsoft YaHei UI Light",14,"bold"))
 spinbox = Spinbox(app, from_=0, to=100, width=5, relief="sunken", repeatdelay=500, repeatinterval=100,
-                     bg="#0e1111",fg="#FAF9F6",font=("Microsoft YaHei UI Light",13,"bold"))
+                     bg="#030f1e",fg="#FAF9F6",font=("Microsoft YaHei UI Light",13,"bold"))
 # Setting options for the Spinbox
-spinbox.config(state="normal", cursor="hand2", bd=3, justify="center", wrap=True,buttonbackground="#0e1111",buttoncursor="hand2")
+spinbox.config(state="normal", cursor="hand2", bd=0, justify="center", wrap=True,buttonbackground="#030f1e",buttoncursor="hand2")
 login = Button(app,text="Log In",bg="#880085",fg="#FAF9F6",font=("serif",13,"bold"))
+underline = Frame(bg="#FAF9F6",width=250,height=3)
 
-header.place(x=275,y=30)
-user.place(x=220,y=100)
-drop.place(x=240,y=140)
-age.place(x=220,y=190)
-spinbox.place(x=280,y=190,)
-login.place(x=220,y=240,width=180)
+header.place(x=355,y=20)
+user.place(x=270,y=100)
+drop.place(x=290,y=145)
+age.place(x=270,y=190)
+spinbox.place(x=330,y=193)
+login.place(x=300,y=240,width=180)
+underline.place(x=270,y=130)
 
 #to generate the application
 app.mainloop()
