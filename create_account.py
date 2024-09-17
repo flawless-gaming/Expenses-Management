32c3f5c972b983187560e27f1ba9a11536c1c701
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-17 23:58:58 +0530

    Add files via upload

--- create_account.py
+++ create_account.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 def create_page():
     app = Toplevel()
-    
+    app.title("Create Account")
+    app.geometry("400x400")
+    app.config(bg="#030f1e")
     
     app.mainloop()
