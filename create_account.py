8a13d0cbc39d537be3afedb5b7638b9bbccbb2f3
Rishi Jain <76516951+flawless-gaming@users.noreply.github.com>
2024-09-18 23:58:37 +0530

    Add files via upload

--- create_account.py
+++ create_account.py
@@ -1,9 +1,17 @@
 from tkinter import *
+from tkvideo import tkvideo
 
 def create_page():
-    app = Toplevel()
+    app = Tk()
     app.title("Create Account")
-    app.geometry("400x400")
+    app.geometry("643x365")
     app.config(bg="#030f1e")
     
+    bg_video = Label(app)
+    bg_video.place(x=0,y=0)
+    bg_player = tkvideo("video.mp4",bg_video,loop=1)
+    bg_player.play()
+    
     app.mainloop()
+    
+create_page()
